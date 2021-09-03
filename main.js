98db4028000df7f55ba5a6d10c9fcabf28346eae
Diganta165 <mdsrdiganta@gmail.com>
2021-09-03 10:17:15 +0600

    Complete

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const API_KEY = `864cfc9671f5a3bb9178fba1f44cbfc0`;
 const searchTemperature = ( ) =>{
     const inputField = document.getElementById('input-field');
     const inputFieldValue = inputField.value;
-    console.log(inputFieldValue);
+    // console.log(inputFieldValue);
 
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputFieldValue}&appid=${API_KEY}&units=metric`;
@@ -16,7 +16,23 @@ const searchTemperature = ( ) =>{
 // Display Weather
 
 const displayWeather = (data) =>{
-    console.log(data);
-    const city = document.getElementById('city');
-    city.innerText = `${data.name}`;
+    // console.log(data);
+    // const city = document.getElementById('city');
+    // city.innerText = `${data.name}`;
+    setInnerText('city', data.name);
+    
+    setInnerText('temperature', data.main.temp);
+
+    setInnerText('leads', data.weather[0].main);
+    // console.log('Data Weather', data.weather[0].main);
+
+    // Set Icon 
+    const url = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const weatherIcon = document.getElementById('weather-icon');
+    weatherIcon.setAttribute('src', url);
+}
+
+// Set Innertext With ID 
+const setInnerText = (id, text) =>{
+    document.getElementById(id).innerText = text;
 }
